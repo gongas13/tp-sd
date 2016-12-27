e67d6a97492a6ca7a38a7c80cddb05eea0652d9e
mets22 <nuno.mets@hotmail.com>
2016-12-27 21:43:46 +0000

    ai vai o trabalho que fiz ontem

--- tp-sd/src/Servidor/Leilao.java
+++ tp-sd/src/Servidor/Leilao.java
@@ -7,41 +7,47 @@ import java.util.concurrent.locks.ReentrantLock;
 
 public class Leilao implements Serializable{
   private int id, maiorofer;
-  private String descricao, maioruti;
-  private HashMap<String,int> lista;
+  private String descricao, maioruti, dono;
+  private HashSet<String> lista;
 
-  public Leilao(int id,String descricao){
+  public Leilao(int id, String dono, String descricao){
     this.id = id;
+    this.dono = dono;
     this.descricao = descricao;
     this.maiorofer = 0;
     this.maioruti = "";
-    this.lista = new HashMap<>();
+    this.lista = new HashSet();
   }
 
   public Leilao(Leilao l){
     this.id = l.getId();
+    this.dono = l.getDono();
     this.descricao = l.getDescricao();
     this.maiorofer = l.getMaiorOferta();
     this.maioruti = l.getMaiorUti();
-    this.lista = new HashMap<>(l.getLista());
+    this.lista = new HashSet(l.getLista());
   }
 
   public int getId(){
     return this.id;
   }
 
+  public int getDono(){
+    return this.dono;
+  }
+
   public String getDescricao(){
     return this.descricao;
   }
 
-  public Map<String,int> getLista(){
-    Map<String,int> aux = new HashMap<>(this.lista);
+  public Set<String> getLista(){
+    Set<String> aux = new HashSet(this.lista);
     return aux;
   }
 
   public void insereLicitacao(int oferta, String nome){
-    this.lista.put(nome,oferta);
     if(oferta>this.maiorofer) {
+      this.lista.put(this.maiorouti,this.maiorofer);
       this.maiorofer = oferta;
       this.maioruti = nome;
     }
@@ -55,15 +61,31 @@ public class Leilao implements Serializable{
     return this.maioruti;
   }
 
-  public String toString(){
+  public String getDetalhes(){
     StringBuilder sb = new StringBuilder();
     sb.append("Id: ");
     sb.append(this.id.toString());
     sb.append("\nDescricao: ");
     sb.append(this.descricao);
-    sb.append("\n");
     sb.append("\nOferta: ");
     sb.append(this.maiorofer.toString());
+    sb.append("\n");
+
+    return sb.toString();
+  }
+
+  public String toString(){
+    StringBuilder sb = new StringBuilder();
+    sb.append("Id: ");
+    sb.append(this.id.toString());
+    sb.append("\nDono");
+    sb.append(this.dono);
+    sb.append("\nDescricao: ");
+    sb.append(this.descricao);
+    sb.append("\nOferta: ");
+    sb.append(this.maiorofer.toString());
+    sb.append("\nLicitador: ");
+    sb.append(this.maioruti);
     sb.append("\n");
 
     return sb.toString();
@@ -78,7 +100,7 @@ public class Leilao implements Serializable{
     if(l == null || this.getClass() != l.getClass()) return false;
     else{
       Leilao aux = (Leilao) l;
-      return (this.getId().equals(l.getId()) && this.getDescricao().equals(l.getDescricao()) && this.getMaiorOferta().equals(l.getMaiorOferta()) && this.getMaiorUti().equals(l.getMaiorUti()));
+      return (this.getId().equals(l.getId()) && this.getDono().equals(l.getDono()) && this.getDescricao().equals(l.getDescricao()) && this.getMaiorOferta().equals(l.getMaiorOferta()) && this.getMaiorUti().equals(l.getMaiorUti()));
     }
   }
 }
--- tp-sd/src/Servidor/Leiloes.java
+++ tp-sd/src/Servidor/Leiloes.java
@@ -5,20 +5,78 @@ import java.util.HashMap;
 import java.util.concurrent.locks.ReentrantLock;
 
 public class Leiloes implements Serializable{
-  ReentrantLock lock;
-  HashMap<String, Leilao> leiloesemcurso;
-  HashMap<String, Leilao> leiloesterminados;
+  private ReentrantLock lock;
+  private int ids;
+  private HashMap<int,Leilao> leiloesemcurso;
+  private HashMap<int,Leilao> leiloesterminados;
 
   public Leiloes(){
     this.lock = new ReentrantLock();
-    leiloesemcurso = new HashMap<>();
-    leiloesterminados = new HashMap<>();
+    this.leiloesemcurso = new HashMap<>();
+    this.leiloesterminados = new HashMap<>();
   }
 
-  public Leiloes(){
+  public Leiloes(Leiloes l){
     this.lock = new ReentrantLock();
-    leiloesemcurso = new HashMap<>();
-    leiloesterminados = new HashMap<>();
+    this.leiloesemcurso = l.getLeiloesEmCurso();
+    this.leiloesterminados = l.getLeiloesTerminados();
+  }
+
+  public void insereLeilao(String utilizador, String descricao){
+    this.lock.lock();
+    try{
+      Leilao l = new Leilao(this.ids, utilizador, descricao);
+      this.leiloesemcurso.put(this.ids++,l);
+    }
+    finally{
+      this.lock.unlock();
+    }
+  }
+
+  public String fechaLeilao(int id, String utilizador){
+    this.lock.lock();
+    try{
+
+    }
+    finally{
+      this.lock.unlock();
+    }
+  }
+
+  public void licitar(int id, String utilizador, int oferta){
+    this.lock.lock();
+    try{
+      String aux;
+      Leilao l = this.leiloesemcurso.get(id).clone();
+      if(l.getMaiorOferta() < oferta) aux = "A oferta tem de ser maior que a existente\n";
+      else {
+        l.insereLicitacao(oferta,utilizador);
+        aux = "Inserido com sucesso";
+      }
+    }
+    finally{
+      this.lock.unlock();
+      return aux;
+    }
+  }
+
+  public String listaEmCurso(String utilizador){
+    this.lock.lock();
+    try{
+      TreeSet aux = this.leiloesemcurso.values();
+      Iterator it = aux.iterator();
+      StringBuilder sb = new StringBuilder();
+        while(it.hasNext()){
+          Leilao l = it.next();
+          sb.append(l.getDetalhes());
+          if(l.getMaiorUti().equals(utilizador)) sb.append("Actualmente tem a maior oferta!\n");
+          else if(l.getDono().equals(utilizador)) sb.append("É o dono!\n");
+        }
+    }
+    finally{
+      this.lock.unlock();
+      return sb.toString();
+    }
   }
 
   public String toString(){
@@ -38,6 +96,4 @@ public class Leiloes implements Serializable{
   public Leilao clone(){
     return new Leilao(this);
   }
-
-  
 }
